0956b34604c71030057a6bf12d2b0bdb486d62f9
edinaldoagrogis <edinaldoconsultar@gmail.com>
2026-08-28 12:16:12 -0300

    Fix CORS for Capacitor APK

--- api/login.js
+++ api/login.js
@@ -7,6 +7,7 @@ if (redisUrl) {
 }
 
 export default async function handler(req, res) {
+    if (req.method === 'OPTIONS') return res.status(200).end();
     if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
     
     try {
